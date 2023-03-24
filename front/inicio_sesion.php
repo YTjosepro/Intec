46ea9aea246c1f3973ee3cfd10fc29a6fc982878
surita <suritabtc@gmail.com>
2023-03-24 14:35:41 -0500

    agregado boton de pagina a tutoriales y corregidos errores de maquetado

--- front/inicio_sesion.php
+++ front/inicio_sesion.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <title>Iniciar sesión</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src='main.js'></script>
     <style>
         img {
@@ -11,13 +13,14 @@
         }
     </style>
 </head>
+
 <body>
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
                 <img src="../fotos/imginicio.png" class="img-fluid">
             </div>
-            
+
             <div class="col-md-6">
                 <div class="row justify-content-center align-items-center">
                     <div class="col-12 col-md-8 col-lg-6">
@@ -31,15 +34,12 @@
                             </div>
                             <div class="form-group">
                                 <label for="contrasena">Contraseña</label>
-                                <input type="password" class="form-control" id="contrasena" name="txtpass" placeholder="******">
+                                <input type="password" class="form-control" id="contrasena" name="txtpass"
+                                    placeholder="******">
                             </div>
                             <div>
-                                <form action="tutoriales.php" class="d-inline-block">
-                                    <button type="submit" class="btn btn-info mr-2">Tutoriales</button>
-                                </form>
-                                <form action="index.php" class="d-inline-block">
-                                    <button type="submit" class="btn btn-info">Ingresar</button>
-                                </form>
+                                <a href="tutoriales.php" class="btn btn-info mr-2">Tutoriales</a>
+                                <button type="submit" class="btn btn-info">Ingresar</button>
                             </div>
                         </form>
                     </div>
@@ -48,7 +48,14 @@
         </div>
     </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+    crossorigin="anonymous"></script>
+
 </html>
