8ee8cb9e0b5d55f6e535dc20dd1563b0e7c6d46e
surita <suritabtc@gmail.com>
2023-03-24 11:52:13 -0500

    correccion de error

--- front/inicio_sesion.php
+++ front/inicio_sesion.php
@@ -2,48 +2,51 @@
 <html>
 <head>
     <meta charset='utf-8'>
-    <title>Iniciar_sesion</title>
+    <title>Iniciar sesión</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src='main.js'></script>
+    <style>
+        img {
+            max-width: 100%;
+        }
+    </style>
 </head>
 <body>
     <div class="container-fluid">
-        <div class="row" >
-            <div class="col-6">
-            <img style="width:700px; height: 625px;" src="../fotos/imginicio.png">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="../fotos/imginicio.png" class="img-fluid">
             </div>
             
-            <div class="col-6">
-                <center>
-                <div class="row">
-                    <div class="col">
-                        <img style="height: 200px; margin-bottom: 30px;" src="../fotos/logo.png">
+            <div class="col-md-6">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12 col-md-8 col-lg-6">
+                        <img src="../fotos/logo.png" class="img-fluid mb-3">
+                        <h3>IntecSofware</h3>
+                        <h7>Software de control técnico administrativo</h7>
+                        <form method="POST" action="index.php">
+                            <div class="form-group mt-4">
+                                <label for="usuario">Usuario</label>
+                                <input type="text" class="form-control" id="usuario" placeholder="name@domain.com">
+                            </div>
+                            <div class="form-group">
+                                <label for="contrasena">Contraseña</label>
+                                <input type="password" class="form-control" id="contrasena" name="txtpass" placeholder="******">
+                            </div>
+                            <div>
+                                <form action="tutoriales.php" class="d-inline-block">
+                                    <button type="submit" class="btn btn-info mr-2">Tutoriales</button>
+                                </form>
+                                <form action="index.php" class="d-inline-block">
+                                    <button type="submit" class="btn btn-info">Ingresar</button>
+                                </form>
+                            </div>
+                        </form>
                     </div>
                 </div>
-                <h3>IntecSofware</h3>
-                <h7>Sofware de control tegnico<br>Administrativo</h7>
-                <form method="POST" action="index.php">
-                <tr>
-                    <h6 style="margin-top: 20px">Usuario</h6>
-                    <input type="text" placeholder="name@domain.com">
-                </tr>
-                <tr>
-                    <h6 style="margin-top: 20px">Contraseña</h6>
-                    <input type="password" name="txtpass" placeholder="******">
-                </tr>
-
-
-                <div class="col">
-                    <button type="submit" class="btn btn-info" style="margin-top:20px">tutoriales</button>
-                    <button type="submit" class="btn btn-info" style="margin-top:20px; margin-left:10px">Ingresar</button>
-                </div>
-                </form>
-                </center>
             </div>
         </div>
     </div>
-
-
 </body>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
